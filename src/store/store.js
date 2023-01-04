5c76c14233804107d1a089e15758bae523118f7a
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-04 19:50:48 +0300

    feat: added redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,7 @@ import { createStore } from 'redux';
 import taskReducer from './tasks';
 
 function configureStore() {
-  return createStore(taskReducer)
+  return createStore(taskReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
 
 export default configureStore;
